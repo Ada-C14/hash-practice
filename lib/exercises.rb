bd0c7ec1ea923aaf32d4c4aa946e7afb106cd7ff
Jasmine <jasylop@gmail.com>
2021-04-13 21:53:35 -0700

    implement group anagrams

--- lib/exercises.rb
+++ lib/exercises.rb
@@ -1,11 +1,24 @@
 
 # This method will return an array of arrays.
 # Each subarray will have strings which are anagrams of each other
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n * klog(k)) where n is the number of strings and k is the length of each string. 
+# Space Complexity: O(n) - using hash to store each word. 
+# Does having an array as a key increase the space complexity?
 
 def grouped_anagrams(strings)
-  raise NotImplementedError, "Method hasn't been implemented yet!"
+  # Create a hash table
+  anagrams = {}
+  # Keys will be sorted word
+  # values will be the arrays of words that are anagrams of sorted word. 
+  strings.each do |word|
+    sorted_word = word.split('').sort.join
+    if anagrams.has_key?(sorted_word)
+      anagrams[sorted_word].push(word)
+    else
+      anagrams[sorted_word] = [word]
+    end
+  end
+  return anagrams.values
 end
 
 # This method will return the k most common elements
