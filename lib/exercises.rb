2a298ef2f7295d8ed6a96cb0b9f7280b8d0ed2f8
MonaRahmani <monarahmanimehr@gmail.com>
2021-04-17 19:18:28 -0700

    solved first and second

--- lib/exercises.rb
+++ lib/exercises.rb
@@ -1,19 +1,55 @@
 
 # This method will return an array of arrays.
 # Each subarray will have strings which are anagrams of each other
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 
-def grouped_anagrams(strings)
-  raise NotImplementedError, "Method hasn't been implemented yet!"
+def grouped_anagrams(strings) 
+  grouped_anagrams = []
+  hash_table = {}
+
+  strings.each do |word|
+    letters = word.split("").sort!  
+
+    if hash_table.key?(letters)
+      hash_table[letters] << word
+    else 
+      hash_table[letters] = [word]
+    end 
+  end 
+
+  hash_table.each do |k, v|
+    grouped_anagrams << v 
+  end 
+
+  return grouped_anagrams
 end
 
 # This method will return the k most common elements
 # in the case of a tie it will select the first occuring element.
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: o(n)
+# Space Complexity: o(n)
 def top_k_frequent_elements(list, k)
-  raise NotImplementedError, "Method hasn't been implemented yet!"
+  # raise NotImplementedError, "Method hasn't been implemented yet!"
+  return [] if list.empty?
+
+  result = []
+  hash = Hash.new(0)
+
+  list.each do |num|
+      hash[num] += 1
+  end
+
+  # sort values in descending order
+  sorted_hash = hash.sort_by {|k,v| -v}  
+  # [[1, 3], [2, 2], [3, 1]]
+
+  i = 0
+  while i < k do 
+      result << sorted_hash[i][0]
+      i += 1
+  end
+  return result   
 end
 
 
