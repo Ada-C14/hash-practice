9ba3eb962762e83c31a3c9b8b0fdd69ee11d7fb6
Hanh Solo <minhhanhnyc@gmail.com>
2021-05-24 11:07:58 -0700

    grouped anagrams

--- lib/exercises.rb
+++ lib/exercises.rb
@@ -1,11 +1,38 @@
 
 # This method will return an array of arrays.
 # Each subarray will have strings which are anagrams of each other
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n*mlog(m)) where n is the number of words and m is 
+# is the size of the longest word in string O(mlogm) is the result of the sorting
+# Space Complexity: n * m (worst case has to create a new array/key for each string and takes m space to sort it)
 
 def grouped_anagrams(strings)
-  raise NotImplementedError, "Method hasn't been implemented yet!"
+  result_array = []
+  anagram_hash = {}
+  
+  if strings.length == 0 || strings.nil?
+    return result_array
+  end
+  # iterate through strings to build a hash table
+  # the hash table where key is the sorted string and value is the array of group anagrams
+  strings.each_with_index do |string, index|
+  # split strings into chars
+  # sort chars and rejoin to create stored string
+    sorted_string = string.chars.sort.join
+    if anagram_hash.key?(sorted_string)
+      # if sorted string is already in the array
+      # add word to the group anagram array
+      anagram_hash[sorted_string].push(strings[index])
+    else
+      # otherwise, set anagram hash value to a new empty array
+      # to account for potential anagrams
+      anagram_hash[sorted_string] = [strings[index]] 
+    end
+  end
+  # iterate through anagram hash and return anagram array
+  anagram_hash.values.each do |anagrams|
+    result_array.push(anagrams)
+  end
+  return result_array
 end
 
 # This method will return the k most common elements
