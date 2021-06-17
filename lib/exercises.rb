f7eb2d304212b9a55573b0dfa3c6e2aa6d06ba77
Beatrice T <be@trice.codes>
2021-06-16 21:33:35 -0700

    finished assignment

--- lib/exercises.rb
+++ lib/exercises.rb
@@ -41,10 +41,45 @@ end
 
 # This method will return the k most common elements
 # in the case of a tie it will select the first occuring element.
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n log n) - O(n) to build map, O(n) to reverse,
+# O(n log n) to sort, O(n) to find answer where n is amount of numbers
+# Space Complexity: O(n) - Similarly to last time, there's a decent overhead but it scales linearly
 def top_k_frequent_elements(list, k)
-  raise NotImplementedError, "Method hasn't been implemented yet!"
+  return [] if list == []
+
+  # Build hash map of number -> occurrences
+  num_map = Hash.new(0)
+  list.each do |num|
+    num_map[num] += 1
+  end
+
+  # Reverse it
+  key_map = Hash.new([])
+  num_map.each do |key, value|
+    if key_map[value] == []
+      key_map[value] = [key]
+    else
+      key_map[value].push(key)
+    end
+  end
+
+  # Sort the keys
+  keys = key_map.keys.sort
+
+  # Go through sorted keys grabbing numbers from key_map until we have enough
+  num_found = 0
+  answer_array = []
+  until num_found >= k
+    num = keys.pop
+    found_nums = key_map[num]
+    num_found += found_nums.length
+    found_nums.each do |num|
+      answer_array.push(num)
+    end
+  end
+
+  # Cut off excess numbers
+  return answer_array[0..k-1]
 end
 
 
