31fb7ce404047ad3bc2f9a12c6fc24a6b29c2a80
Beatrice T <be@trice.codes>
2021-06-16 21:15:56 -0700

    anagrams

--- lib/exercises.rb
+++ lib/exercises.rb
@@ -1,11 +1,42 @@
 
 # This method will return an array of arrays.
 # Each subarray will have strings which are anagrams of each other
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n) where n is number of strings
+# O(n) to build strings_map, O(n) to build reverse_map, O(n) to build answer_array
+# Space Complexity: O(n) where n is number of strings
+# While there is a large amount of data structures, they scale linearly
 
 def grouped_anagrams(strings)
-  raise NotImplementedError, "Method hasn't been implemented yet!"
+  # Put strings into hash map of string -> # of occurrences of character
+  # Luckily {a:1, b:2} == {b:2, a:1}
+  strings_map = {}
+  strings.each do |string|
+    letter_map = Hash.new(0)
+    string.each_char do |char|
+      letter_map[char] += 1
+    end
+
+    strings_map[string] = letter_map
+
+  end
+
+  # Reverse the hash
+  reverse_map = Hash.new([])
+  strings_map.each do |key, value|
+    if reverse_map[value] == []
+      reverse_map[value] = [key]
+    else
+      reverse_map[value].push(key)
+    end
+  end
+
+  # Build answer array
+  answer_array = []
+  reverse_map.each do |key, value|
+    answer_array.push(value)
+  end
+
+  return answer_array
 end
 
 # This method will return the k most common elements
