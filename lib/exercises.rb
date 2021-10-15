4eb68199731de441ae92e64c781783f580313105
Noor B.A <noor.bahr.aluloom@gmail.com>
2021-10-15 10:06:18 -0700

    finalized sudoku validation

--- lib/exercises.rb
+++ lib/exercises.rb
@@ -50,9 +50,74 @@ end
 #   a valid sudoku table.
 #   Each element can either be a ".", or a digit 1-9
 #   The same digit cannot appear twice or more in the same 
-#   row, column or 3x3 subgrid
-# Time Complexity: ?
-# Space Complexity: ?
+#   row, column or the 9 3x3 subgrid
+# Time Complexity: O(1) for a 9 block sudoku
+# Space Complexity: O(n) for the hash table
 def valid_sudoku(table)
-  raise NotImplementedError, "Method hasn't been implemented yet!"
+  if table.nil? || table.empty?
+    raise ArgumentError,'no table provided'
+  end
+
+  # row check
+  table.each do |row|
+    row_hash = {}
+    row.each do |input|
+      if validate_sudoku_input(input)
+        if row_hash[input]
+          false
+        else
+          row_hash[input] = true
+        end
+      end
+    end
+  end
+
+  # column check
+  i = 0
+  while i < 9
+    col_hash = {}
+    table.each do |row|
+      input = row[i]
+      if validate_sudoku_input(input)
+        if col_hash[input]
+          false
+        else
+          col_hash[input] = true
+        end
+      end
+    end
+    i += 1
+  end
+
+  # 9 3x3 block
+  [0,3,6].each do |rows| # created array for rows
+    [0,3,6].each do |columns| # created array for columns
+      return false unless sudoku_3x3_block(rows, columns, table)
+    end
+  end
+
+  true
+end
+
+
+def validate_sudoku_input(value)
+  if value != "." && value =~ /[^123456789]/
+    false
+  end
+end
+
+def sudoku_3x3_block(rows, columns, table)
+  block_hash = {}
+  (rows...rows+3).each do |row|
+    (columns...columns+3).each do |column|
+      input = table[row][column]
+      return false unless validate_sudoku_input(input)
+      if block_hash[input]
+        false
+      else
+        block_hash[input] = true
+      end
+    end
+  end
+  true
 end
--- lib/test.rb
+++ lib/test.rb
@@ -1,29 +1,5 @@
-# def find_judge(n, trust)
-#     relations = Hash.new {|h,k| h[k] = {outdegree: 0 , indegree: 0}}
-    
-#     trust.each do |a,b|
-#         relations[a] = {
-#             outdegree: + 1
-#         }
-#         relations[b] = {
-#             indegree: + 1
-#         }
-#     end
-    
-#     judge = relations.select{|relation, hash| hash[indegree] == n - 1 && hash[outdegree] == 0}
-    
-#     # print relations
-#     return judge
-    
-# end
+array = ['a','b','c','d']
 
-# puts find_judge(2, [1,2])
-
-# test = {indegree: 0,
-# outdegree: 0}
-
-# print test
-
-h = Hash.new { |h,k| h[k] = { count: 0, rating: 0 } }
-puts h
-h
+[0,2].each do |num|
+    puts num
+end
